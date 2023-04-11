3d158d740b3f45319e6d712809bc9ac1c3248d96
Manuel Herdez <herdez@gmail.com>
2023-04-11 12:32:47 -0600

    click, dblclick event

--- app.js
+++ app.js
@@ -23,28 +23,9 @@ const answers = [
   },
 ]
 
-
-const $ = selector => document.querySelector(selector)
-
-
-$scoreGame  = $('#score-game')
-$scoreTeam1 = $('#score-team1')
-$scoreTeam2 = $('#score-team2')
-
-$equis1 = $('#equis1')
-$equis2 = $('#equis2')
-$equis3 = $('#equis3')
-
-// $scoreGame.textContent = '100'
-// $scoreTeam1.textContent = '200'
-// $scoreTeam2.textContent = '80'
-
-// $answer1.textContent = null
-// $points1.textContent = null
-// $points1.classList.remove('answer-points')
-
 const showAnswer = ( answerNum ) => {
   if( answerNum <= 0 || answerNum > 4 ) return
+
   const {answer, points} = answers[answerNum-1]
   const $answer = $(`#answer${answerNum}`)
   const $points = $(`#points${answerNum}`)
@@ -54,4 +35,44 @@ const showAnswer = ( answerNum ) => {
   $points.textContent = points;
 }
 
-showAnswer(1)
+const hiddenAnswer = ( answerNum ) => {
+  console.log(answerNum)
+  if( answerNum <= 0 || answerNum > 4 ) return
+
+  const $answer = $(`#answer${answerNum}`)
+  const $points = $(`#points${answerNum}`)
+
+  $answer.textContent = null;
+  $points.classList.remove('answer-points');
+  $points.textContent = null;
+}
+
+const $ = selector => document.querySelector(selector)
+
+
+const $scoreGame  = $('#score-game')
+const $scoreTeam1 = $('#score-team1')
+const $scoreTeam2 = $('#score-team2')
+
+const $equis1 = $('#equis1')
+const $equis2 = $('#equis2')
+const $equis3 = $('#equis3')
+
+
+const $answers = document.querySelectorAll('.answer')
+
+
+$answers.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    showAnswer(i+1);
+  })
+  btn.classList.add('cursor')
+});
+
+$answers.forEach((btn, i) => {
+  btn.addEventListener('dblclick', () => {
+    hiddenAnswer(i+1);
+  })
+  btn.classList.add('cursor')
+});
+
